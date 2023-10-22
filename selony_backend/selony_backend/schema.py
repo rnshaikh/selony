fa7644f981ce06df4c0806fa289abcd02ba612da
Rizwan Shaikh <rshaikh@ces-ltd.com>
2023-10-23 01:09:52 +0530

    added root schema

--- selony_backend/selony_backend/schema.py
+++ selony_backend/selony_backend/schema.py
@@ -1,13 +1,15 @@
 import graphene
 
-from user_management.schema import UserQueries, Mutation
+from user_management.schema.queries.user import UserQueries
+from user_management.schema.queries.address import AddressQueries
+from user_management.schema.mutations.user_auth import UserAuthMutation
 
 
-class Query(UserQueries):
+class Query(UserQueries, AddressQueries):
     pass
 
 
-class Mutation(Mutation):
+class Mutation(UserAuthMutation):
     pass
 
 
